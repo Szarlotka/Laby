21a46a24b2ab96d62da194aff1230aff6125b6bf
Szarlotka <leon.charlott@gmail.com>
2017-01-23 16:44:20 +0100

    Update Labyrinthe.java

--- Labyrinthe.java
+++ Labyrinthe.java
@@ -5,7 +5,9 @@ public class Labyrinthe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Grille grille1 = new Grille(5,5);
+		Grille grille1 = new Grille();
+		grille1.affGrille();
+		grille1.laby();
 		grille1.affGrille();
 	}
 
