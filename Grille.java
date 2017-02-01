1feaa587c7441b0d11c3f9b6cc9a9698a4491f75
Szarlotka <leon.charlott@gmail.com>
2017-02-01 15:10:41 +0100

    Update Grille.java

--- Grille.java
+++ Grille.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //import Case.Etat;
 
 
@@ -7,7 +9,7 @@ public class Grille {
 	private int longueur;
 
 	public Grille(int hauteur, int longueur){
-		int val=0;
+		int val=-1;
 		this.hauteur=hauteur;
 		this.longueur=longueur;
 		grille = new Case[hauteur][longueur];
@@ -21,7 +23,7 @@ public class Grille {
 	}
 
 	public Grille(){
-		this(5,5);
+		this(4,4);
 	}
 
 	public void affGrille(){
@@ -50,10 +52,12 @@ public class Grille {
 		System.out.println("+");
 	}
 
-	public void laby(){
-		//while (toutChiffre()==false){
-		for (int x=0; x<10; x++){
+	/*public void laby(){
+		//int nbMursTombes=0;
+		while (/*nbMursTombes!=hauteur*longueur-1*//*toutChiffre()==false){
 			//determination du mur à faire tomber
+
+
 			int m=(int)Math.round(Math.random());
 			//tirage aleatoire de la case, h pr le i, l pour le j de la case
 			int h, l;
@@ -66,8 +70,9 @@ public class Grille {
 				//si les deux cases n'ont pas la meme valeur, le mur peut être ouvert
 				if (grille[h][l].getVal()!=grille[h-1][l].getVal()){
 					grille[h][l].setMurH(Case.Etat.OUVERT);
+					//nbMursTombes++;
 					grille[h-1][l].setVal(grille[h][l].getVal());
-					etendreChiffre_rec(h-1,l);
+					etendreChiffre(grille[h-1][l].getVal(), grille[h][l].getVal());
 				}
 			}else{
 				System.out.println("mur gauche");
@@ -76,13 +81,75 @@ public class Grille {
 				System.out.println(h+", "+l);
 				if (grille[h][l].getVal()!=grille[h][l-1].getVal()){
 					grille[h][l].setMurG(Case.Etat.OUVERT);
+					//nbMursTombes++;
 					grille[h][l-1].setVal(grille[h][l].getVal());
-					etendreChiffre_rec(h,l-1);
+					etendreChiffre(grille[h][l].getVal(), grille[h][l-1].getVal());
 				}
 			}
 			affGrille();
 		}
+	}*/
+
+	public void laby(){
+		//Mur mur=new Mur(0,0,0);
+		ArrayList<Mur> mursRestants = new ArrayList<Mur>(); 
+		for (int i=0; i<hauteur; i++){
+			for (int j=0; j<longueur; j++){
+				if (j!=0){
+					if (grille[i][j].getMurG()==Case.Etat.FERME && grille[i][j-1]!=grille[i][j]){
+						Mur mur=new Mur(i,j,0);
+						mursRestants.add(mur);
+					}
+				}
+				if (i!=0){
+					if (grille[i][j].getMurH()==Case.Etat.FERME && grille[i-1][j]!=grille[i][j]){
+						Mur mur=new Mur(i,j,1);
+						mursRestants.add(mur);
+					}
+				}
+			}
+		}
+
+		int nbMurs;
+		int choixMur;
+		int murSituation;
+		int murX;
+		int murY;
+		//m=1 : mur haut, m=0 mur gauche
+		while (toutChiffre()==false){
+			nbMurs=mursRestants.size();
+			//System.out.println("nb de murs restants:"+nbMurs);
+			choixMur=(int)Math.round(Math.random()*(nbMurs-1));
+			//System.out.println(choixMur);
+			murSituation=mursRestants.get(choixMur).getMurM();
+			murX=mursRestants.get(choixMur).getMurX();
+			murY=mursRestants.get(choixMur).getMurY();
+			int valAvant=grille[murX][murY].getVal();
+			if (murSituation==1){
+				if (grille[murX][murY].getVal()==grille[murX-1][murY].getVal()){
+					mursRestants.remove(choixMur);
+				}else{
+
+					//System.out.println("mur haut");
+					grille[murX][murY].setMurH(Case.Etat.OUVERT);
+					grille[murX][murY].setVal(grille[murX-1][murY].getVal());
+					etendreChiffre(grille[murX-1][murY].getVal(), valAvant);
+					mursRestants.remove(choixMur);
+				}
+			}else{
+				if (grille[murX][murY].getVal()==grille[murX][murY-1].getVal()){
+					mursRestants.remove(choixMur);
+				}else{
+					//System.out.println("mur gauche");
+					grille[murX][murY].setMurG(Case.Etat.OUVERT);
+					grille[murX][murY].setVal(grille[murX][murY-1].getVal());
+					etendreChiffre(grille[murX][murY-1].getVal(), valAvant);
+					mursRestants.remove(choixMur);
+				}
+			}
+		}
 	}
+
 
 	public boolean toutChiffre(){
 		int a = grille[0][0].getVal();
@@ -95,26 +162,22 @@ public class Grille {
 		}
 		return(true);
 	}
-	
-	//a revoir
-	public void etendreChiffre_rec(int x, int y){
-		if (grille[x][y].getMurH()==Case.Etat.OUVERT && grille[x][y].getVal()!=grille[x][y-1].getVal()){
-			grille[x][y-1].setVal(grille[x][y].getVal());
-			System.out.println("a");
+
+	public void etendreChiffre(int val1, int val2){
+		int valavant=val2;
+		int valnouv=val1;
+		if (val1>val2){
+			valavant=val1;
+			valnouv=val2;
 		}
-		if (grille[x][y-1].getMurH()==Case.Etat.OUVERT && grille[x][y-1].getVal()!=grille[x][y-2].getVal()){
-			etendreChiffre_rec(x, y-1);
-			System.out.println("b");
+		for (int i=0; i<hauteur; i++){
+			for (int j=0; j<longueur; j++){
+				if (grille[i][j].getVal()==valavant){
+					grille[i][j].setVal(valnouv);
+				}
+			}
 		}
-		if (grille[x][y].getMurG()==Case.Etat.OUVERT && grille[x][y].getVal()!=grille[x-1][y].getVal()){
-			grille[x-1][y].setVal(grille[x][y].getVal());
-			System.out.println("c");
-		}
-		if (grille[x-1][y].getMurG()==Case.Etat.OUVERT && grille[x-1][y].getVal()!=grille[x-2][y].getVal()){
-			etendreChiffre_rec(x-1, y);
-			System.out.println("d");
-		}
-	}
-	
-	
+	}	
+
+
 }
