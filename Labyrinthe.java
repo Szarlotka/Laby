3c3631b9b208ea6c5d5350b7f05e2a922b08f177
Szarlotka <leon.charlott@gmail.com>
2017-02-02 09:41:14 +0100

    Update Labyrinthe.java
    labyrinthe console fonctionnant

--- Labyrinthe.java
+++ Labyrinthe.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Labyrinthe {
 
@@ -5,12 +6,31 @@ public class Labyrinthe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Grille grille1 = new Grille(8,8);
-	//	grille1.affGrille();
+		System.out.println("choisissez la taille du labyrinthe:");
+		System.out.println("nombre de cases en longueur:");
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		System.out.println("nombre de cases en hauteur:");
+		int h = sc.nextInt();
+		Grille grille1 = new Grille(h,l);
 		grille1.laby();
-	//	grille1.affGrille();
 		grille1.afflaby();
-	//	System.out.println(grille1.toutChiffre());
+
+		while (grille1.gagner()!=true){
+			String direction = "a";
+			while (!direction.equals("d") && !direction.equals("q") && !direction.equals("z") && !direction.equals("s")){
+				System.out.println("Où voulez-vous aller?");
+				System.out.println("(droite:d, Gauche:q, Haut:z, Bas:s)");
+				direction = sc.next();
+			}
+			//grille1.bougerPion(direction);
+			if (grille1.bougerPion(direction)){
+				grille1.afflaby();
+			}else{
+				System.out.println("vous ne pouvez pas aller par là !");
+			}
+		}
+		System.out.println("Vous avez gagné !");
 	}
 
 }
